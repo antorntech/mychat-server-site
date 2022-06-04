86d587eabb9f38e9176b3f008116cc63237f228e
antor10655gh <creativefreelancerantor@gmail.com>
2022-06-05 00:22:07 +0600

    add all file

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const socketIO = require("socket.io");
 const app = express();
 const port = process.env.PORT || 5000;
 
+const users = [{}];
+
+app.use(cors());
+
 app.get("/", (req, res) => {
   res.send("Hi");
 });
@@ -14,8 +18,32 @@ const server = http.createServer(app);
 
 const io = socketIO(server);
 
-io.on("connection", () => {
+io.on("connection", (socket) => {
   console.log("New Connection is Ready");
+  socket.on("joined", ({ user }) => {
+    users[socket.id] = user;
+    console.log(`${user} has joined`);
+    socket.broadcast.emit("userJoined", {
+      user: "Admin",
+      message: `${users[socket.id]} has joined`,
+    });
+    socket.emit("welcome", {
+      user: "Admin",
+      message: `Welcome to MyChat ${users[socket.id]}`,
+    });
+  });
+
+  socket.on("message", ({ message, id }) => {
+    io.emit("sendMessage", { user: users[id], message, id });
+  });
+
+  //   socket.on("disconnect", () => {
+  //     socket.broadcast.emit("leave", {
+  //       user: "Admin",
+  //       message: `${users[socket.id]}  has left`,
+  //     });
+  //     console.log(`user left`);
+  //   });
 });
 
 server.listen(port, () => {
